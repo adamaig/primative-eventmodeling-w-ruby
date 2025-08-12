a952d5cbc93b206671a92e6d27abf2b6a6869b85
Adam Ingram-Goble <adamaig@gmail.com>
2025-08-12 14:10:14 +0000

    Add focus filter to rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,7 @@ require 'faker'
 
 # Require all *.rb files in lib directory
 Dir[File.expand_path('../lib/**/*.rb', __dir__)].each { |file| require file }
+
+RSpec.configure do |config|
+  config.filter_run_when_matching :focus
+end
