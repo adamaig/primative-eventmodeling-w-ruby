10107145039db3525142650c5ed2392b47db017f
Adam Ingram-Goble <adamaig@gmail.com>
2025-08-15 05:21:53 +0000

    Add YARD documentation for cart command and event classes
    Document all command structs in commands.rb with attribute and usage details
    Add YARD docs for event classes in domain_events.rb including attributes and example

--- lib/simple_event_modeling/cart/commands.rb
+++ lib/simple_event_modeling/cart/commands.rb
@@ -2,11 +2,49 @@
 
 module SimpleEventModeling
   module Cart
+    # Cart Commands for SimpleEventModeling
+    #
+    # This module defines command objects for cart operations in the event modeling example.
+    # Each command is an immutable value object representing an intent to change cart state.
+    #
+    # @example Add an item to cart
+    #   AddItem.new(cart_id: 'abc', item_id: 'sku123', quantity: 2)
+    #
     module Commands
-      Unknown = Struct.new(:aggregate_id)
+      # Command to create a new cart
+      #
+      # @!attribute [rw] aggregate_id
+      #   @return [String] The aggregate identifier
+      # @example
+      #   CreateCart.new('cart-123')
       CreateCart = Struct.new(:aggregate_id)
+
+      # Command to add an item to the cart
+      #
+      # @!attribute [rw] aggregate_id
+      #   @return [String] The cart identifier
+      # @!attribute [rw] item_id
+      #   @return [String] The item identifier
+      # @example
+      #   AddItem.new('cart-123', 'sku123')
       AddItem = Struct.new(:aggregate_id, :item_id)
+
+      # Command to remove an item from the cart
+      #
+      # @!attribute [rw] aggregate_id
+      #   @return [String] The cart identifier
+      # @!attribute [rw] item_id
+      #   @return [String] The item identifier
+      # @example
+      #   RemoveItem.new('cart-123', 'sku123')
       RemoveItem = Struct.new(:aggregate_id, :item_id)
+
+      # Command to clear all items from the cart
+      #
+      # @!attribute [rw] aggregate_id
+      #   @return [String] The cart identifier
+      # @example
+      #   ClearCart.new('cart-123')
       ClearCart = Struct.new(:aggregate_id)
     end
   end
--- lib/simple_event_modeling/cart/domain_events.rb
+++ lib/simple_event_modeling/cart/domain_events.rb
@@ -5,24 +5,60 @@ require 'securerandom'
 module SimpleEventModeling
   module Cart
     module DomainEvents
+      # Event representing cart creation
+      #
+      # @!attribute [r] aggregate_id
+      #   @return [String] The cart identifier
+      # @!attribute [r] version
+      #   @return [Integer] The event version (always 1)
+      # @example
+      #   CartCreated.new(aggregate_id: 'cart-123')
       class CartCreated < SimpleEventModeling::Common::Event
         def initialize(aggregate_id:)
           super(type: self.class, aggregate_id: aggregate_id, version: 1)
         end
       end
 
+      # Event representing an item being added to the cart
+      #
+      # @!attribute [r] aggregate_id
+      #   @return [String] The cart identifier
+      # @!attribute [r] version
+      #   @return [Integer] The event version
+      # @!attribute [r] item_id
+      #   @return [String] The item identifier
+      # @example
+      #   ItemAdded.new(aggregate_id: 'cart-123', version: 2, item_id: 'sku123')
       class ItemAdded < SimpleEventModeling::Common::Event
         def initialize(aggregate_id:, version:, item_id:)
           super(type: self.class, aggregate_id: aggregate_id, version: version, data: { item: item_id })
         end
       end
 
+      # Event representing an item being removed from the cart
+      #
+      # @!attribute [r] aggregate_id
+      #   @return [String] The cart identifier
+      # @!attribute [r] version
+      #   @return [Integer] The event version
+      # @!attribute [r] item_id
+      #   @return [String] The item identifier
+      # @example
+      #   ItemRemoved.new(aggregate_id: 'cart-123', version: 3, item_id: 'sku123')
       class ItemRemoved < SimpleEventModeling::Common::Event
         def initialize(aggregate_id:, version:, item_id:)
           super(type: self.class, aggregate_id: aggregate_id, version: version, data: { item: item_id })
         end
       end
 
+      # Event representing the cart being cleared
+      #
+      # @!attribute [r] aggregate_id
+      #   @return [String] The cart identifier
+      # @!attribute [r] version
+      #   @return [Integer] The event version
+      # @example
+      #   CartCleared.new(aggregate_id: 'cart-123', version: 4)
       class CartCleared < SimpleEventModeling::Common::Event
         def initialize(aggregate_id:, version:)
           super(type: self.class, aggregate_id: aggregate_id, version: version)
